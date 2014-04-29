a08cf11602dcb4542824067441a92c806666dfdf
edward <e4lam@yahoo.com>
2014-04-29 00:37:08 -0400

    Update docs

--- docs/group__macros.js
+++ docs/group__macros.js
@@ -1,10 +1,11 @@
 var group__macros =
 [
+    [ "XLKIT_API", "group__macros.html#ga06b437451f78e10f3b12c1b5b9fc44d6", null ],
     [ "XLKIT_BEGIN_FUNCTION", "group__macros.html#gafd2dac36bad5ee2dcabe0fc520cb56de", null ],
-    [ "XLKIT_END_FUNCTION", "group__macros.html#ga5dd19d673fadf4005c0c22a8275958e6", null ],
-    [ "XLKIT_EXPORT", "group__macros.html#ga22f05100ebd892237b434154d2205ef4", null ],
+    [ "XLKIT_END_FUNCTION", "group__macros.html#gad39b49cebaed3956ae025cda567ce0b6", null ],
     [ "XLKIT_INIT_ADDIN_LABEL", "group__macros.html#gac7516c97350b41ce21391dd94d71be75", null ],
     [ "XLKIT_PARM", "group__macros.html#ga8c0e5cc3a5cb8f7d6c3a9d0193bdf359", null ],
+    [ "XLKIT_PRAGMA_DLL_EXPORT", "group__macros.html#gaa0282a0f99bd539c621a24c8426af393", null ],
     [ "XLKIT_REGISTER", "group__macros.html#gaf36e4b338360ea733a813dff3714b5b0", null ],
     [ "XLKIT_REGISTER_AS", "group__macros.html#gaea2ea76c50f469f9d9a87ecdc7954ac9", null ]
 ];
